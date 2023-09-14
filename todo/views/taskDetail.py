f45f7635a8d861a66819145f7b238732fd3804f5
Amábile <amabile.analistadesistemas@gmail.com>
2023-09-14 20:25:14 -0300

    retornar uma única categoria

--- todo/views/taskDetail.py
+++ todo/views/taskDetail.py
@@ -3,20 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from todo.models.category import CategoryEntity
 from todo.models.task import TaskEntity
 from todo.serializers.task import TaskSerializer
-
 
 class TaskDetailView(APIView):
     """
     Retrieve, update or delete a category instance.
     """
-    def get_object(self, pk):
-        pass
+    def get_task(self, pk):
+        try:
+            return TaskEntity.objects.get(pk=pk)
+        except TaskEntity.DoesNotExist:
+            raise Http404
 
     def get(self, request, pk, format=None):
-        tasks = TaskEntity.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
+        task = self.get_task(pk)
+        serializer = TaskSerializer(task)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
@@ -24,4 +27,3 @@ class TaskDetailView(APIView):
 
     def delete(self, request, pk, format=None):
         pass
-    
